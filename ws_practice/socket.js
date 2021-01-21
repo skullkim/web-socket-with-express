ee2054439c5873b15910eb3f8b72812cf342de9c
김윤기 <iskull@gim-yungiui-MacBookPro.local>
2021-01-21 16:21:13 +0900

    ws

--- ws_practice/socket.js
+++ ws_practice/socket.js
@@ -2,18 +2,18 @@ const WenSocket = require('ws');
 
 module.exports = (server) => {
     const wss = new WenSocket.Server({server});
-    wss.on('connection', (ws, req) => {
+    wss.on('connection', (ws, req) => {//When connecting to web socket
         //The way to figure out client IP
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         console.log('access to new client', ip);
-        ws.on('message', (message) => {
+        ws.on('message', (message) => {//When receiving message from client
             console.log(message);
         });
-        ws.on('error', (error) => {
+        ws.on('error', (error) => {//When coccuring error
             console.error(error);
             clearInterval(ws.interval);
         });
-        ws.on('close', () => {
+        ws.on('close', () => {//When disconnecting 
             console.log('disconnect client', ip);
         });
 
